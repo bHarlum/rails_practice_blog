fd08842db9bd8172b1548d58e144c4f268e85df1
Bryce harlum <bryce.harlum@gmail.com>
2019-04-12 10:24:49 +1000

    Added index page, controller methods and routes.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+    def home
+        redirect_to index_path
+    end
+    def index
+    end
+    def new
+    end
+    def update
+    end
+    def show
+    end
+    def destroy
+    end
+    def create
+    end
+    def edit
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/", to: "posts#home" 
+  get "/posts", to: "posts#index", as: "index"
+  get "/post/:id", to: "posts#show"
+  get "/post/new", to: "posts#new"
+  post "/posts", to: "posts#create", as: "create_post"
+  get "/posts/:id/edit", to: "posts#edit"
+  put "/post/:id/edit", to: "posts#update"
+  patch "/posts/:id/edit", to: "posts#update"
+  delete "/posts/:id", to: "posts#destroy"
 end
